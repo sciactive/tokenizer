233ffbd766245fa0f7ce3de5337572b5265a4035
Hunter Perrin <hperrin@gmail.com>
2025-11-08 14:20:32 -0800

    feat: add tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
+import { createDefaultEsmPreset } from 'ts-jest';
+
+const presetConfig = createDefaultEsmPreset();
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const jestConfig = {
+  ...presetConfig,
   testEnvironment: 'node',
   rootDir: 'src/',
+  moduleNameMapper: {
+    '^(\\.|\\.\\.)\\/(.+)\\.js': '$1/$2',
+  },
 };
+
+export default jestConfig;
